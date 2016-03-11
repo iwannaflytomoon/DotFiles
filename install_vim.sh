544edf7c6113da83bb699922278565a743f509a3
iwannaflytomoon <iwannaflytomoon@gmail.com>
2016-03-11 17:53:37 +0800

    1) now script will asked for installation directory 2) script will test whether the installation directory can be written, otherwise add "sudo" before "make install" 3) to ask whether install vimdoc

--- install_vim.sh
+++ install_vim.sh
@@ -1,20 +1,22 @@
  #!/bin/sh
 
 version=7.4
-binary_error=""
+is_error=0
 software_path="$HOME/.softwareCache/"
 url_prefix="ftp://ftp.vim.org/pub/vim/unix/"
+intsall_path="/usr/local/vim"
 
+PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
-BLUE='\033[0;34m'
 RED='\033[0;31m'
+BLUE='\033[0;34m'
 NC='\033[0m' # No Color]]]]'
 
 download() {
 	file_name=$(basename $1)
 	file_path="${software_path}${file_name}"
 	if [ ! -e $file_path ]; then
-		printf "$BLUE Downloading vim ...$NC\n"
+		printf "$RED Downloading $1 ...$NC\n"
 		mkdir -p $software_path
 		cd $software_path
 		if which wget > /dev/null; then
@@ -23,10 +25,11 @@ download() {
 			curl -fL $1
 		else
 			printf "$RED wget or curl not found...$NC\n"
+			is_error=1	
 			return 1
 		fi
 	else
-		printf "$BLUE vim install packages already exists...$NC\n"
+		printf "$RED $1 already exists...$NC\n"
 	fi
 }
 
@@ -41,20 +44,78 @@ install_vim() {
         sed -i "" '1i\
         #include <AvailabilityMacros.h>
         ' src/os_unix.h
-	else
-		sudo yum install ncurses-devel zlib-devel openssl-devel sqlite-devel
+	#else
+		#sudo yum install ncurses-devel zlib-devel openssl-devel sqlite-devel
 	fi
 
-	./configure --prefix=/usr/local/vim \
+	printf "$CYAN Where do you want to install?(Default:/usr/local/vim)$NC "
+	read answer
+	if [ -d $answer ]; then
+		if [ -n "$answer" ]; then
+			install_path=$answer
+		fi
+	else
+		printf "$RED input path error!$NC\n"
+	    is_error=1
+		return 1
+	fi 
+	
+	#judge install_path writeable
+	touch $install_path/.test_writeable 
+	if [ 0 -eq $? ]; then
+		is_writeable=1
+		rm -f $install_path/.test_writeable
+	else
+		is_writeable=0
+	fi
+	./configure --prefix=${install_path} \
 				--with-features=huge \
-				--with-compiledby="iwannaflytomoon" \
+				--with-compiledby="kenan" \
 				--enable-multibyte \
 				--enable-pythoninterp=yes \
 				--enable-cscope \
 				--enable-fontset \
-				--enable-rubyinterp
-	make
-	sudo make install
+				--enable-rubyinterp \
+				--quiet
+	make --quiet distclean
+	make --quiet clean
+	make --quiet
+	if [ 1 -eq $is_writeable ]; then
+		make --quiet install
+	else
+		sudo make install
+	fi
+
+	if [ 0 -ne $? ]; then
+		printf "$RED vim install failed!$NC\n"
+		is_error=1
+	fi
+}
+
+ask() {
+	printf  "$CYAN $1 ([y]/n) $NC  "
+	read answer
+	[[ $answer =~ ^[Nn]$ ]]
+}
+
+install_vimdoc() {
+	printf "$PURPLE install vimdoc...$NC\n"
+	url="http://sourceforge.net/projects/vimcdoc/files/vimcdoc/vimcdoc-1.9.0.tar.gz"
+	download $url
+	file_name=$(basename $url)
+	vimdoc_dir="vimdoc"
+	cd $software_path
+	mkdir -p $vimdoc_dir
+	tar zxf $file_name --strip-components 1 -C ${vimdoc_dir}
+	cd ${vimdoc_dir}
+	./vimcdoc.sh -I
+
+	if [ 0 -ne $? ]; then
+		printf "$RED vimdoc install failed!$NC\n"
+		is_error=1
+	else
+		printf "$RED vimdoc install succecc!$NC\n"
+	fi
 }
 
 archi=$(uname -sm)
@@ -63,13 +124,20 @@ case "$archi" in
 	Darwin\ x86_64) download "${url_prefix}vim-${version}.tar.bz2";;
 esac
 
-if [ ! -n "$binary_error" ]; then
+if [ 0 -eq $is_error ]; then
 case "$archi" in
 	Linux\ x86_64) install_vim 0;;
 	Darwin\ x86_64) install_vim 1;;
 esac
-	printf "$BLUE vim install success!$NC\n"
+fi
+if [ 0 -eq $is_error ]; then
+	printf "$RED vim install success!!!$NC\n"
 else
-	printf "$RED - $binary_error !!!$NC\n"
-	exit 1
+	printf "$RED vim install fail!!!$NC\n"
+	exit
+fi
+
+ask "Do you want to vim's chinese doc?"
+if [ 1 -eq $? ]; then
+	install_vimdoc
 fi
